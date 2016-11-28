29a1b7f53787cd944b42733ac6e762b4a5f1f692
Katja Karhu <katja.karhu@procountor.com>
2016-11-28 12:30:17 +0200

    Rename Django project

--- hapi-api/settings.py
+++ hapi-api/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'hateapi.urls'
+ROOT_URLCONF = 'hapi-api.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'hateapi.wsgi.application'
+WSGI_APPLICATION = 'hapi-api.wsgi.application'
 
 
 # Database
