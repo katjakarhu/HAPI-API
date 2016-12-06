d48927da7fcbb0da9dd39e9ba7205b8b7fd9da59
Katja Karhu <katja.karhu@iki.fi>
2016-12-06 12:11:25 +0200

    Project structure fixed

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
  FROM python:3.5.2
  ENV PYTHONUNBUFFERED 1
+ RUN apt-get update
+ RUN apt-get install --quiet --assume-yes postgresql-client
  RUN mkdir /code
  WORKDIR /code
- ADD requirements.txt /code/
+ ADD hapi-api/requirements.txt /code/
  RUN pip install -r requirements.txt
  ADD . /code/
---
+++ docker-command.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Postgres must be up and accepting command
+# So, let's check for that before doing anything else
+set -e
+
+host=db
+cmd=5432
+export PGPASSWORD="docker"
+
+until psql -h "$host" -U "docker" -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Yay! Postgres is up - activating project"
+
+# Collect static files
+echo "Collect static files"
+#python hapi-api/manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python hapi-api/manage.py migrate
+
+# Start server
+echo "Starting server"
+python hapi-api/manage.py runsslserver 0.0.0.0:8000
